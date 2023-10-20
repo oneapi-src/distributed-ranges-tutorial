0de43719e73a87fa01d7b84c638886ef9cdab0d1
Kacper Stefański <kacper.stefanski99@gmail.com>
2023-10-20 14:57:01 +0200

    added a docker_workflow.yml

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -4,13 +4,13 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-source /opt/intel/oneapi/setvars.sh
-set -e
-hostname
+# source /opt/intel/oneapi/setvars.sh
+# set -e
+# hostname
 
-# SLURM/MPI integration is broken
-unset SLURM_TASKS_PER_NODE
-unset SLURM_JOBID
+# # SLURM/MPI integration is broken
+# unset SLURM_TASKS_PER_NODE
+# unset SLURM_JOBID
 
 cmake -B build -DENABLE_SYCL=on
 cmake --build build -j
