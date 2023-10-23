5a585e58333510484f6d21ec9d315862ac4fcc10
Kacper Stefański <kacper.stefanski99@gmail.com>
2023-10-23 15:57:08 +0200

    small fixes

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -4,14 +4,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# source /opt/intel/oneapi/setvars.sh
-# set -e
-# hostname
-
-# # SLURM/MPI integration is broken
-# unset SLURM_TASKS_PER_NODE
-# unset SLURM_JOBID
-
 cmake -B build -DENABLE_SYCL=on
 cmake --build build -j
 
